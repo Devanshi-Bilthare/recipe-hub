9a42e99440eee3e31a55b6e67c6579b29f704321
Devanshi Bilthare <100258554+Devanshi-Bilthare@users.noreply.github.com>
2024-06-21 17:22:32 +0530

    first commit

---
+++ models/userSchema.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const plm = require('passport-local-mongoose')
+
+const userSchema = new mongoose.Schema({
+    profilepic:{
+        type:String,
+        default:'image.png'
+    },
+    name:{
+        type:String,
+    },
+    username:{
+        type:String,
+    },
+    email:{
+        type:String,
+        required:[true,"Email is required"],
+        RegExp:['/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/',"provide a valid email"]
+    },
+    bio:{
+        type:String
+    },
+    password:{
+        type:String
+    },
+    recepie:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'recepie'
+    }],
+    saved: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'recepie'
+    }],
+    follower:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'user'
+    }]
+})
+
+userSchema.plugin(plm)
+
+const user = mongoose.model('user',userSchema)
+
+module.exports = user
